4b38a6f40f0afe36fcdbb308b5c4c24e8f6c3b3f
Arash <47273556+arashnrim@users.noreply.github.com>
2024-04-01 16:47:35 +0800

    feat: implement table view

---
+++ Objective Flashcards/Controllers/HomeViewController.m
@@ -0,0 +1,61 @@
+//
+//  ViewController.m
+//  Objective Flashcards
+//
+//  Created by Arash on 29/3/24.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Encourages the app to have large titles where possible
+    self.navigationController.navigationBar.prefersLargeTitles = YES;
+    
+    // Sets the delegate and data source of the table view to the VC
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+}
+
+// Determines the number of rows to fill in the table view. A required implementation under UITableViewDataSource.
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+// Returns the table cell at the index specified. A required implementation under UITableView.
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.textLabel.text = [NSString stringWithFormat:@"Cell %ld", (long)indexPath.row];
+    
+    return cell;
+}
+
+// Handles the event when users tap the row at the index specified.
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UIAlertController *alert = [UIAlertController 
+                                alertControllerWithTitle:@"YIPPEE"
+                                message:[NSString stringWithFormat:@"The cell at the index %ld was selected.", (long)indexPath.row]
+                                preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *actionDismiss = [UIAlertAction 
+                                    actionWithTitle:@"YIPPEE"
+                                    style:UIAlertActionStyleDefault
+                                    handler:nil];
+    
+    [alert addAction:actionDismiss];
+    
+    [self presentViewController:alert animated:YES completion: nil];
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 126.0;
+}
+
+
+@end
